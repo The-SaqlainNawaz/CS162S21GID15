1593fc637e1682e42481685afbad489566ad22d6
Sharjeel-Iftikhar <isharjeel63@gmail.com>
2021-05-24 00:55:36 +0500

    Changing Gallery

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let img = document.querySelector('#img1');
+let img2 = document.querySelector('#img2');
+let img3 = document.querySelector('#img3');
+let img4 = document.querySelector('#img4');
+let img5 = document.querySelector('#img5');
+let img6 = document.querySelector('#img6');
+let img7 = document.querySelector('#img7');
+let img8 = document.querySelector('#img8');
+let img9 = document.querySelector('#img9');
+let btt1 = document.querySelector('#btt1');
+let btt2 = document.querySelector('#btt2');
+let btt3 = document.querySelector('#btt3');
+
+btn1.addEventListener('click', ()=>{
+    img.src = 'Images/image5.jpg';
+    img2.src = 'Images/image1.jpg';
+})
+
+btn2.addEventListener('click', ()=>{
+    img.src = 'Images/image3.jpg';
+})
+
+btn3.addEventListener('click', ()=>{
+    img.src = 'Images/image2.jpg';
+})
