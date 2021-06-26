f0627b27a590967056c76e94efb9d999a95ff25c
The-SaqlainNawaz <pacsaqiirr@gmail.com>
2021-06-26 18:45:11 +0500

    working

---
+++ AutomotiQ/WebApplication1/Controllers/AdminAddVehiclesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+
+    public class AdminAddVehiclesController : ApiController
+    {
+        
+        [HttpPost]
+        public bool AdminAddVeh(Vehicles veh)
+        {
+            AddVehicleEntities2 dbo = new AddVehicleEntities2();
+            string s = veh.idno.ToString();
+            try
+            {
+                AddVehicle vehic = new AddVehicle();
+                vehic.VehicleName = veh.name;
+                vehic.Color = veh.color;
+                vehic.Price = veh.price;
+                vehic.Model = veh.brand;
+                vehic.Capacity = veh.enginecap;
+                vehic.Category = veh.catagory;
+                vehic.ID = s;
+                vehic.Stck = veh.stck;
+                dbo.AddVehicles.Add(vehic);    
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            dbo.SaveChanges();
+            return true;
+        }
+        [HttpGet]
+        public List<Vehicles> getVehiclesInfo()
+        {
+            List<Vehicles> Vehicles = new List<Vehicles>();
+            AddVehicleEntities2 addVehicleEntities = new AddVehicleEntities2();
+            var list = addVehicleEntities.AddVehicles.ToList();
+            foreach(var dbo in list)
+            {
+                Vehicles v = new Vehicles();
+                v.name = dbo.VehicleName;
+                v.color = dbo.Color;
+                v.price = dbo.Price;
+                v.brand = dbo.Model;
+                v.enginecap = dbo.Capacity;
+                v.catagory = dbo.Category;
+                v.idno = dbo.ID;
+                v.stck = dbo.Stck;
+                Vehicles.Add(v);
+            }
+            return Vehicles;
+        }
+
+
+    }
+}
--- AutomotiQ/WebApplication1/Models/Vehicles.cs
+++ AutomotiQ/WebApplication1/Models/Vehicles.cs
@@ -10,11 +10,13 @@ namespace WebApplication1.Models
         public string name { get; set; }
         public string brand { get; set; }
         public string color { get; set; }
-        public int price { get; set; }
-        public int stck { get; set; }
-        public Byte picture { get; set; }
-        public int idno { get; set; }
+        public string price { get; set; }
+        public string stck { get; set; }
+        public string enginecap { get; set; }
+        
+        public string idno { get; set; }
         public string catagory { get; set; }
+      //  public Byte picture { get; set; }
 
     }
 }
